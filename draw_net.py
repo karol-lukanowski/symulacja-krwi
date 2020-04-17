20cae8519bfcec323e4a6d647bc9cd4d6287c3c0
karol-lukanowski <62500795+karol-lukanowski@users.noreply.github.com>
2020-04-17 19:37:08 +0200

    Add files via upload

--- draw_net.py
+++ draw_net.py
@@ -3,64 +3,87 @@
 
 import matplotlib.pyplot as plt
 import networkx as nx
+import numpy as np
 
 # normalizacja rysowania (maksymalna grubość krawędzi)
-qdrawconst = 5
+qdrawconst = 10
 ddrawconst = 3
 
-def drawq(G, n, name, qdrawconst = qdrawconst, normalize = True):
+
+def drawq(G, n, name, qdrawconst=qdrawconst, normalize=True, in_nodes=[], out_nodes=[]):
     """
     rysowanie przepływów
     """
     plt.figure(figsize=(10, 10))
     pos = nx.get_node_attributes(G, 'pos')
-    #nx.draw(G, pos, node_size=0, with_labels=False)
-    
-    qmax = 0
-    for edge in G.edges(data='q'):
-        if (edge[2] > qmax):
-            qmax=edge[2]
 
     if (normalize == False):
         qmax = 1
         qdrawconst = 1
-    Ed = []
-    Qu = []
+    else:
+        qmax = max([edge[2] for edge in G.edges(data='q')])
+
+    edges = []
+    qs = []
     for edge in G.edges(data='q'):
         if not ((edge[0] % n == 0 and edge[1] % n == n - 1) or (edge[1] % n == 0 and edge[0] % n == n - 1)):
-            x,y,q = edge
-            Ed.append((x,y))
-            Qu.append(q)
-    nx.draw_networkx_edges(G, pos, edgelist=Ed, width=qdrawconst * Qu / qmax)
-    
+            x, y, q = edge
+            edges.append((x, y))
+            qs.append(q)
+    nx.draw_networkx_edges(G, pos, edgelist=edges, width=qdrawconst * np.array(qs) / qmax)
+
+    #### IN_NODES i OUT_NODES ####
+    x_in, y_in = [], []
+    for node in in_nodes:
+        x_in.append(pos[node][0])
+        y_in.append(pos[node][1])
+    plt.scatter(x_in, y_in, s=60, facecolors='white', edgecolors='black')
+
+    x_out, y_out = [], []
+    for node in out_nodes:
+        x_out.append(pos[node][0])
+        y_out.append(pos[node][1])
+    plt.scatter(x_out, y_out, s=60, facecolors='black', edgecolors='white')
+
     plt.axis('equal')
     plt.savefig(name)
     plt.close()
 
-def drawd(G, n, name, ddrawconst = ddrawconst, normalize = True):
+
+def drawd(G, n, name, ddrawconst=ddrawconst, normalize=True, in_nodes=[], out_nodes=[]):
     """
     rysowanie srednic
     """
     plt.figure(figsize=(10, 10))
     pos = nx.get_node_attributes(G, 'pos')
-    #nx.draw(G, pos, node_size=0)
-    
-    dmax = 0
-    for edge in G.edges(data='d'):
-        if (edge[2] > dmax):
-            dmax=edge[2]
 
     if (normalize == False):
         dmax = 1
         ddrawconst = 1
-    Ed = []
-    Dia = []
+    else:
+        dmax = max([edge[2] for edge in G.edges(data='d')])
+
+    edges = []
+    ds = []
     for edge in G.edges(data='d'):
         if not ((edge[0] % n == 0 and edge[1] % n == n - 1) or (edge[1] % n == 0 and edge[0] % n == n - 1)):
-            x,y,d = edge
-            Ed.append((x,y))
-            Dia.append(d)
-    nx.draw_networkx_edges(G, pos, edgelist=Ed, width=qdrawconst * Dia / dmax)
+            x, y, d = edge
+            edges.append((x, y))
+            ds.append(d)
+    nx.draw_networkx_edges(G, pos, edgelist=edges, width=ddrawconst * np.array(ds) / dmax)
+
+    #### IN_NODES i OUT_NODES ####
+    x_in, y_in = [], []
+    for node in in_nodes:
+        x_in.append(pos[node][0])
+        y_in.append(pos[node][1])
+    plt.scatter(x_in, y_in, s=60, facecolors='white', edgecolors='black')
+
+    x_out, y_out = [], []
+    for node in out_nodes:
+        x_out.append(pos[node][0])
+        y_out.append(pos[node][1])
+    plt.scatter(x_out, y_out, s=60, facecolors='black', edgecolors='white')
 
     plt.axis('equal')
     plt.savefig(name)
